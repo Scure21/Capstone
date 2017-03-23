c3dd2731786db3c1e839567467d83cc7a1f277a2
Christine <cleverett1@gmail.com>
2017-03-22 20:30:52 -0400

    redirects for local

--- app/components/Login.jsx
+++ app/components/Login.jsx
@@ -4,6 +4,7 @@ export const Login = ({ login }) => (
   <form onSubmit={evt => {
     evt.preventDefault()
     login(evt.target.username.value, evt.target.password.value)
+    browserHistory.push('/interstitial')
   } }>
     <input name="username" />
     <input name="password" type="password" />
