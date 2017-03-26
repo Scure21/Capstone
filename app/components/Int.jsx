b87394c4d9acb1236fcc9329f6fa698732a4c161
Christine <cleverett1@gmail.com>
2017-03-26 17:46:52 -0400

    logo and byline added

--- app/components/Int.jsx
+++ app/components/Int.jsx
@@ -6,6 +6,10 @@ export default class Int extends Component {
   render () {
     return (
   <div id="welcome-page">
+    <div id="logo">
+      <img src="logo.png" className="img-responsive" id="logoName"/>
+      <img src="banner.png" className="img-responsive" id="banner"/>
+    </div>
     <h1>hello</h1>
   </div>
     )
