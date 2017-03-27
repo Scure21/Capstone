6d49be9ddac26bcbb04c57771c921858c04fc97f
Christine <cleverett1@gmail.com>
2017-03-27 10:20:20 -0400

    merge-conflict

--- app/components/Controller.jsx
+++ app/components/Controller.jsx
@@ -1,114 +1,105 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 import io from 'socket.io-client'
-var socket = io.connect('http://192.168.0.8:1337')
-var device
-    device = window.navigator.userAgent
+import { connect } from 'react-redux'
 
+const socket = io.connect('http://192.168.1.184:1337')
+const device = window.navigator.userAgent
 
-export default class Controller extends Component{
+export default class Controller extends Component {
+ constructor (props) {
+   super(props)
 
-    constructor(props){
-        super(props)
+   this.moveUp = this.moveUp.bind(this)
+   this.moveDown = this.moveDown.bind(this)
+   this.moveLeft = this.moveLeft.bind(this)
+   this.moveRight = this.moveRight.bind(this)
+   this.powerUp = this.powerUp.bind(this)
+ }
 
-        this.moveUp = this.moveUp.bind(this)
-        this.moveDown = this.moveDown.bind(this)
-        this.moveLeft = this.moveLeft.bind(this)
-        this.moveRight = this.moveRight.bind(this)
-        this.powerUp = this.powerUp.bind(this)
-    }
+ componentDidMount () {
+     // when the component mounts on the mobile device, we add the user to the State with a new Snake instance
 
-    this.moveUp = this.moveUp.bind(this)
-    this.moveDown = this.moveDown.bind(this)
-    this.moveLeft = this.moveLeft.bind(this)
-    this.moveRight = this.moveRight.bind(this)
-    this.powerUp = this.powerUp.bind(this)
-  }
+   socket.emit('check-device-type', device)
 
-  componentDidMount () {
-      // when the component mounts on the mobile device, we add the user to the State with a new Snake instance
+       // socket.on('activate-device-controls', function(isPhone) {
+       //         console.log('YES YOU ARE CONNECTED WITH A MOBILE DEVICE')
+       //         var snake_position = {
+       //             x: 0,
+       //             y: 0
+       //         }
 
-    socket.emit('check-device-type', device)
+       // })
 
-        // socket.on('activate-device-controls', function(isPhone) {
-        //         console.log('YES YOU ARE CONNECTED WITH A MOBILE DEVICE')
-        //         var snake_position = {
-        //             x: 0,
-        //             y: 0
-        //         }
+       // var up = document.getElementById('up')
+       // var down = document.getElementById('down')
+       // var left = document.getElementById('left')
+       // var right = document.getElementById('right')
+       // var power = document.getElementById('speed')
 
-        // })
+       // up.addEventListener('touchstart', this.moveUp, false)
+       // down.addEventListener('touchstart', this.moveDown, false)
+       // left.addEventListener('touchstart', this.moveLeft, false)
+       // right.addEventListener('touchstart', this.moveRight, false)
+       // power.addEventListener('touchstart', this.powerUp, false)
+ }
 
-        // var up = document.getElementById('up')
-        // var down = document.getElementById('down')
-        // var left = document.getElementById('left')
-        // var right = document.getElementById('right')
-        // var power = document.getElementById('speed')
+ moveUp () {
+   var data = {
+     x: 0,
+     y: -1
+   }
+   socket.emit('user-movement-update', data)
+ }
 
+ moveDown () {
+   var data = {
+     x: 0,
+     y: 0
+   }
+   socket.emit('user-movement-update', data)
+ }
 
-        // up.addEventListener('touchstart', this.moveUp, false)
-        // down.addEventListener('touchstart', this.moveDown, false)
-        // left.addEventListener('touchstart', this.moveLeft, false)
-        // right.addEventListener('touchstart', this.moveRight, false)
-        // power.addEventListener('touchstart', this.powerUp, false)
+ moveLeft () {
+   var data = {
+     x: 0,
+     y: 0
+   }
+   socket.emit('user-movement-update', data)
+ }
 
+ moveRight () {
+   var data = {
+     x: 0,
+     y: 0
+   }
+   socket.emit('user-movement-update', data)
+ }
 
-  moveUp () {
-    var data = {
-      x: 0,
-      y: -1
-    }
-    socket.emit('user-movement-update', data)
-  }
+ powerUp () {
+   var data = {
+     x: 0,
+     y: 0
+   }
+   socket.emit('user-movement-update', data)
+ }
 
-  moveDown () {
-    var data = {
-      x: 0,
-      y: 0
-    }
-    socket.emit('user-movement-update', data)
-  }
-
-  moveLeft () {
-    var data = {
-      x: 0,
-      y: 0
-    }
-    socket.emit('user-movement-update', data)
-  }
-
-  moveRight () {
-    var data = {
-      x: 0,
-      y: 0
-    }
-    socket.emit('user-movement-update', data)
-  }
-
-  powerUp () {
-    var data = {
-      x: 0,
-      y: 0
-    }
-    socket.emit('user-movement-update', data)
-  }
-
-  render () {
-    return (
-        <div ref="nv">
-            <button id='up' className='ui-btn' className='controller' onClick={this.moveUp}>
-                <span className='glyphicon glyphicon-chevron-up'></span>
-            </button>
-            <button id='left' className='ui-btn' className="controller" onClick={this.moveLeft} >
-                    <span className='glyphicon glyphicon-chevron-left'></span>
-            </button>
-            <button id='right' className='ui-btn' className="controller" onClick={this.moveRight}>
-                    <span className='glyphicon glyphicon-chevron-right'></span>
-            </button>
-            <button id='down' className='ui-btn' className="controller" onClick={this.moveDown}>
-                    <span className='glyphicon glyphicon-chevron-down'></span>
-            </button>
-            <button id='speed' > power </button>
-        </div>
-    )
-  }
+ render () {
+   return (
+       <div ref="nv">
+           <button id='up' className='ui-btn' className='controller' onClick={this.moveUp}>
+               <span className='glyphicon glyphicon-chevron-up'></span>
+           </button>
+           <button id='left' className='ui-btn' className="controller" onClick={this.moveLeft} >
+                   <span className='glyphicon glyphicon-chevron-left'></span>
+           </button>
+           <button id='right' className='ui-btn' className="controller" onClick={this.moveRight}>
+                   <span className='glyphicon glyphicon-chevron-right'></span>
+           </button>
+           <button id='down' className='ui-btn' className="controller" onClick={this.moveDown}>
+                   <span className='glyphicon glyphicon-chevron-down'></span>
+           </button>
+           <button id='speed' > power </button>
+       </div>
+   )
+ }
 }
--- app/game_scripts/sketch.js
+++ app/game_scripts/sketch.js
@@ -5,55 +5,74 @@ import store from '../store'
 import { getSnakes } from '../reducers/snakes'
 
 export default function sketch (p) {
-  var socket
-  var snakes = {}
-  var canvas
-  var foods = []
-  var device
+ var socket
+ var snakes = {}
+ var canvas
+ var foods = []
+ var device
 
-  p.setup = function () {
-    canvas = p.createCanvas(600, 600)
+ p.setup = function () {
+   canvas = p.createCanvas(600, 600)
+   p.frameRate(10)
+   // connect client to the server through sockets
+   socket = io.connect('http://192.168.1.184:1337')
 
-    p.frameRate(1)
-    // in the future this would go in the actual server
-    socket = io.connect('http://192.168.0.8:1337')
+   // // get the device type
+   // device = window.navigator.userAgent
+   // // on connection, emit to the server the device type
+   // socket.emit('check-device-type', device)
 
-    // these 2 lines are from phone_controller
-    device = window.navigator.userAgent
-    console.log('inside of setup device=', device)
-    socket.emit('mobile-device', device)
+   // receives device type from server and if it is a mobile, make a new snake
+   socket.on('send-device-type', function ({deviceType, users}) {
+     if (deviceType === 'mobile') {
+       // loop through the users array and assign each user a new snake
+       users.forEach(user => { snakes[user] = new Snake(null, null, p) })
+       console.log('I AM A MOBILE IN SKETCH!!', snakes)
+       // we are going to have 5 foods on the canvas for all the players
+       for (let i = 0; i < 6; i++) {
+         const food = new Food(p)
+         foods.push(food)
+       }
+       // getting users information to display the scores
+       let _snakes = []
+       for (var id in snakes) {
+         _snakes.push(snakes[id])
+       }
+       store.dispatch(getSnakes(_snakes))
+     }
+   })
 
-    // we get the information from the server of each user movement and we update each user movement
-    socket.on('server-dir-update', function ({data, userId}) {
-      // get the specific user and update the direction of movement
-      // This would need to change because we dont have a single snake anymore, we have an snakes OBJ
-      snakes[userId].dir(data.x, data.y)
-    })
+   // we get the information from the server of each user movement and we update each user movement
+   socket.on('server-dir-update', function ({data, userId}) {
+     // get the specific user and update the direction of movement
+     // This would need to change because we dont have a single snake anymore, we have an snakes OBJ
+     snakes[userId].dir(data.x, data.y)
+   })
 
-    // Handle server disconnection, close the socket connection
-    socket.on('disconnect', function () {
-      socket.close()
-    })
-  }
+   // Handle server disconnection, close the socket connection
+   socket.on('disconnect', function () {
+     socket.close()
+   })
+ }
 
 // -----------------DRAW-------------------- //
-  p.draw = function () {
-    p.background(51)
+ p.draw = function () {
+   p.background(51)
 
-      // Draw each snake
-    for (let id in snakes) {
-      snakes[id].draw(p)
-    }
+     // Draw each snake
+   for (let id in snakes) {
+     snakes[id].draw(p)
+   }
 
-      // Draw the food
-    foods.forEach(food => food.draw(p))
+     // Draw the food
+   foods.forEach(food => food.draw(p))
 
-      // Loop through all the snakes and run the .eat and .move function
-    for (let id in snakes) {
-      snakes[id].move(p)
-      foods.forEach(food => snakes[id].eat(p, food))
-    }
-  }
+     // Loop through all the snakes and run the .eat and .move function
+   for (let id in snakes) {
+     snakes[id].move(p)
+     foods.forEach(food => snakes[id].eat(p, food))
+   }
+ }
 }
 
 // In case we need it tomorrow otherwise we can just delete it
