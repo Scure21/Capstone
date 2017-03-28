f32008a5a343b960d3ab31f37da61062123d06e6
Christine <cleverett1@gmail.com>
2017-03-28 19:30:08 -0400

    most updated "master"

--- app/components/Int.jsx
+++ app/components/Int.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import { browserHistory } from 'react-router'
 import io from 'socket.io-client'
-var socket = io.connect('http://192.168.2.111:1337')
+var socket = io.connect('http://192.168.2.167:1337')
 
 export default class Int extends Component {
 
@@ -18,21 +18,21 @@ export default class Int extends Component {
       this.handleClick = this.handleClick.bind(this)
   }
 
-componentDidMount(){
-  socket.emit('ask-for-users')
-  socket.on('get-current-users', users => {
-    var colors = users.map(function(i){
-      return i.colorName
+  componentDidMount(){
+    socket.emit('ask-for-users')
+    socket.on('get-current-users', users => {
+      var colors = users.map(function(i){
+        return i.colorName
+      })
+      this.setState({users: users, colors: colors})
+      if (this.state.users.length === 4){
+        this.interval = setInterval(this.tick, 1000)
+        this.setState({ready: true})
+      }
     })
-    this.setState({users: users, colors: colors})
-    if (this.state.users.length >= 4){
-      this.interval = setInterval(this.tick, 1000)
-      this.setState({ready: true})
-    }
-  })
-}
- 
-tick() {
+  }
+
+  tick() {
     this.setState({counter: this.state.counter - 1})
     if (this.state.counter <= 0){
       clearInterval(this.interval)
@@ -40,11 +40,12 @@ tick() {
     }
   }
 
-handleClick(){
+  handleClick(){
    this.setState({next: true})
   }
-  
+
   render(){
+    console.log("******* INSIDE INT: ", this.state.users)
         if (this.state.next === false){
           return (
             <div>
@@ -58,27 +59,26 @@ handleClick(){
       </div>
           )
         }
-        
+
         if (this.state.ready === false){
           return (
         <div id="waiting-room">
           <h1>WAITING ROOM</h1>
-        {this.state.colors && 
-            <h2>A 
-          {this.state.colors[this.state.colors.length - 1]} snake has joined!</h2>}
+        {this.state.colors &&
+            <h2>A
+          {' ' + this.state.colors[this.state.colors.length - 1]} snake has joined!</h2>}
         </div>)
       }
       else{
           return (
           <div id="waiting-room">
           <h1>WAITING ROOM</h1>
-        {this.state.colors && 
-            <h2>A 
-          {this.state.colors[this.state.colors.length - 1]} snake has joined!</h2>}
+        {this.state.colors &&
+            <h2>A {' ' + this.state.colors[this.state.colors.length - 1]} snake has joined!</h2>}
         <p><b id="nums">{this.state.counter}</b></p>
         </div>
-        )     
-      }      
+        )
+      }
   }
 }
 
